6200b67c190f39fc388eaee73c5a054913f6bcb7
이주해 <ijuhae@ijuhaeui-MacBookPro.local>
2019-04-11 02:39:28 +0900

    "stack.c"

--- data_structure/stack/stack.c
+++ data_structure/stack/stack.c
@@ -4,57 +4,36 @@
 
 typedef struct stack{
     int input;
-    struct stack* ptr;
+    struct stack* on_ptr;
+    struct stack* under_ptr;
 }stack;
 
-int main(){
-    struct* top_ptr = NULL;
-    int stack_size = 0;
-
-    int inst_cnt = 0;
-    char * instruction = (char *)calloc(20, sizeof(char));
-
-    printf("명령어의 수와 명령어를 입력하세요\n");
-    scanf("%d%*c",&inst_cnt);
-    
-    char ** inst_ptr = (char **)calloc(inst_cnt, sizeof(char *));
-    for(int i = 0 ; i < inst_cnt ; i++)
-        inst_ptr[i] = (char *)calloc(20,sizeof(char));
-
-    for(int i = 0 ; i < inst_cnt ; i++){
-        fgets(inst_ptr[i],20,stdin);
-    }
-
-    for(int i = 0 ; i < inst_cnt ; i++){
-        if(strchr(inst_ptr[i],' ') == NULL)
-            instruction = strtok(inst_ptr[i],"\n");
-        else
-            instruction = strtok(inst_ptr[i]," ");
-
-        if(!strcmp(instruction,"push")){
-            push(top_ptr);
+stack * push(stack * top_ptr, int num){
+    stack * new = (stack *)calloc(1, sizeof(stack));
+    new->on_ptr = NULL;
+    new->under_ptr = top_ptr;
+    new->input = num;
+    if(top_ptr != NULL)
+        top_ptr->on_ptr = new;
+    top_ptr = new;
+    return top_ptr;
+}
+stack * pop(stack * top_ptr, int size){
+    if(size != 0){
+        printf("%d\n",top_ptr->input);
+        if(size != 1){
+            top_ptr = top_ptr->under_ptr;
+            free(top_ptr->on_ptr);
+            top_ptr->on_ptr = NULL;
         }
-        else if(!strcmp(instruction,"pop")){
-            pop();
-        }
-        else if(!strcmp(instruction,"size")){
-            size(stack_size);
-        }       
-        else if(!strcmp(instruction,"empty")){
-            empty(stack_size);
-        }
-        else if(!strcmp(instruction,"top")){
-            top();
+        else{
+            free(top_ptr);
+            top_ptr = NULL;
         }
     }
-    return 0;
-
-}
-void push(int num){
-
-}
-void pop(){
-
+    else
+        printf("-1\n");
+    return top_ptr;
 }
 void size(int stack_size){
     printf("%d\n",stack_size);
@@ -63,8 +42,58 @@ void empty(int stack_size){
     if(stack_size == 0)
         printf("1\n");
     else
-        printf("0"\n);
+        printf("0\n");
 }
-void top(){
+void top(stack * top_ptr, int size){
+    if(size != 0)
+        printf("%d\n",top_ptr->input);
+    else
+        printf("-1\n");
+}
 
+int main(){
+    stack * top_ptr = (stack *)calloc(1, sizeof(stack));
+    top_ptr = NULL;
+    int stack_size = 0;
+    int stack_input = -1;
+    
+    int inst_cnt = 0;
+    char * instruction = (char *)calloc(20, sizeof(char));
+
+    printf("명령어의 수와 명령어를 입력하세요\n");
+    scanf("%d%*c",&inst_cnt);
+    
+    char ** inst_ptr = (char **)calloc(inst_cnt, sizeof(char *));
+
+    for(int i = 0 ; i < inst_cnt ; i++)
+        inst_ptr[i] = (char *)calloc(20,sizeof(char));
+
+    for(int i = 0 ; i < inst_cnt ; i++)
+        fgets(inst_ptr[i],20,stdin);
+
+    for(int i = 0 ; i < inst_cnt ; i++){
+        if(strchr(inst_ptr[i],' ') == NULL)
+            instruction = strtok(inst_ptr[i],"\n");
+        else{
+            instruction = strtok(inst_ptr[i]," ");
+            stack_input = atoi(strtok(NULL," "));
+        }
+
+        if(!strcmp(instruction,"push")){
+            top_ptr = push(top_ptr,stack_input);
+            stack_size ++;
+        }
+        else if(!strcmp(instruction,"pop")){
+            top_ptr = pop(top_ptr,stack_size);
+            if (stack_size >0)
+                stack_size --;
+        }
+        else if(!strcmp(instruction,"size"))
+            size(stack_size);  
+        else if(!strcmp(instruction,"empty"))
+            empty(stack_size);
+        else if(!strcmp(instruction,"top"))
+            top(top_ptr,stack_size);
+    }
+    return 0;
 }
