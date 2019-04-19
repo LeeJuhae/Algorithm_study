3bc33f499c40337383d26303afacc95b2ff727e1
이주해 <ijuhae@ijuhaeui-MacBookPro.local>
2019-04-19 23:09:38 +0900

    complete skilltrees.py

--- programmers/winter_coding/skilltrees.py
+++ programmers/winter_coding/skilltrees.py
@@ -1,2 +1,22 @@
-def solution():
-    
+def solution(skill, skill_trees):
+    cnt = 0
+    for i in range(len(skill_trees)):
+        num = -1
+        change_cnt = 0
+        include = True
+        for j in range(len(skill)):
+            if skill[j] in skill_trees[i]:
+                if include:
+                    if num <skill_trees[i].index(skill[j]) and change_cnt == j:
+                        num = skill_trees[i].index(skill[j])
+                        change_cnt += 1
+                    else:
+                        cnt += 1
+                        break
+                else:
+                    cnt += 1
+                    break
+            else:
+                incude = False
+    answer = len(skill_trees) - cnt
+    return answer
