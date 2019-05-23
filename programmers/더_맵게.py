f7a08bd9708747b6bf9b068499a54ac14b66108c
LeeJuhae <tlatmsdlwngo@naver.com>
2019-05-23 23:13:30 +0900

    Create 더_맵게.py

---
+++ programmers/더_맵게.py
@@ -0,0 +1,11 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while(True):
+        if scoville[0] >= K:
+            return answer
+        elif len(scoville) == 1:
+            return -1
+        heapq.heappush(scoville,heapq.heappop(scoville)+ heapq.heappop(scoville)*2)
+        answer += 1
