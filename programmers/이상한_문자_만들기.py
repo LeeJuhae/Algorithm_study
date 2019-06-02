d4a913c7ba7d54bd54f1cee7892b8553dcb07bc6
LeeJuhae <tlatmsdlwngo@naver.com>
2019-06-02 22:50:59 +0900

    Create 이상한_문자_만들기.py

---
+++ programmers/이상한_문자_만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    ind = 0
+    answer = ''
+    for i in range(len(s)):
+        if s[i] == ' ':
+            ind = 0
+            answer += s[i]
+            continue
+        if ind % 2 == 0:
+            answer += s[i].upper()
+        else:
+            answer += s[i].lower()  
+        ind += 1
+    return answer
