076c1206de87bf6a9705be882bd86b053b02fae7
LeeJuhae <tlatmsdlwngo@naver.com>
2019-06-04 00:24:30 +0900

    Create 이상한_문자_만들기.java

---
+++ programmers/Level1/이상한_문자_만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+  public String solution(String s) {
+      String answer = "";
+      int idx = 0;
+      for(int i = 0 ; i < s.length();i++){
+          if(s.charAt(i) == ' '){
+              idx = 0;
+              answer += Character.toString(s.charAt(i));
+              continue;
+          }
+          if(idx % 2 == 0)
+              answer += Character.toString(s.charAt(i)).toUpperCase();
+          else
+              answer += Character.toString(s.charAt(i)).toLowerCase();
+          idx ++;
+      }
+      return answer;
+  }
+}
