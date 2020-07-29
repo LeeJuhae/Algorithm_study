50141dd23adb0848829bb03534c2cfe04bac766b
LeeJuhae <tlatmsdlwngo@naver.com>
2020-07-29 22:00:41 +0900

    Update 셔틀버스.py

--- programmers/Kakao/셔틀버스.py
+++ programmers/Kakao/셔틀버스.py
@@ -11,7 +11,7 @@ def solution(n, t, m, timetable):
             if timetable[0] <= last_time:
                 last_time = timetable[m-1] - 1
             return '%02d:%02d'%(last_time//60,last_time%60)
+        bus_arrive = (60*9) + i * t
         for j in range(m-1, -1, -1):
-            bus_arrive = (60*9) + i * t
             if timetable[j] <= bus_arrive:
                 del timetable[j]
