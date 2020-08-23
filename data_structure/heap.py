601fdcf4ee4a291655837843dc3eb0140df40e20
LeeJuhae <tlatmsdlwngo@naver.com>
2020-08-23 12:22:11 +0900

    Modify

--- data_structure/heap.py
+++ data_structure/heap.py
@@ -3,8 +3,9 @@ class Node():
 		self.data = data
 
 class Heap():
-	def __init__(self):
+	def __init__(self, isMaxHeap=True):
 		self.queue = [None]
+		self.isMaxHeap = isMaxHeap
 
 	def swap(self, i, j):
 		self.queue[i], self.queue[j] = self.queue[j], self.queue[i]
@@ -14,7 +15,8 @@ class Heap():
 		idx = len(self.queue) - 1
 		while idx > 1:
 			parent_idx = idx // 2
-			if self.queue[parent_idx].data > self.queue[idx].data:
+			if (self.isMaxHeap and self.queue[parent_idx].data < self.queue[idx].data) \
+				or (not self.isMaxHeap and self.queue[parent_idx].data > self.queue[idx].data):
 				self.swap(parent_idx, idx)
 				idx = parent_idx
 			else:
@@ -29,22 +31,32 @@ class Heap():
 		while idx < queue_len:
 			l_child = idx * 2
 			r_child = idx * 2 + 1
-			if l_child < queue_len and self.queue[idx].data > self.queue[l_child].data:
-				self.swap(idx, l_child)
-				idx = l_child
-			elif r_child < queue_len and self.queue[idx].data > self.queue[r_child].data:
-				self.swap(idx, r_child)
-				idx = r_child
-			else:
-				break
+			if self.isMaxHeap:
+				if l_child < queue_len and self.queue[idx].data < self.queue[l_child].data:
+					self.swap(idx, l_child)
+					idx = l_child
+				elif r_child < queue_len and self.queue[idx].data < self.queue[r_child].data:
+					self.swap(idx, r_child)
+					idx = r_child
+				else:
+					break
+			if not self.isMaxHeap:
+				if l_child < queue_len and self.queue[idx].data > self.queue[l_child].data:
+					self.swap(idx, l_child)
+					idx = l_child
+				elif r_child < queue_len and self.queue[idx].data > self.queue[r_child].data:
+					self.swap(idx, r_child)
+					idx = r_child
+				else:
+					break
 		return ret.data
 
-def get_heap(arr):
-	heap = Heap()
+if __name__ == "__main__":
+	arr = [1,3,11,6,5,2]
+	maxHeap = Heap()
+	minHeap = Heap(False)
 	for ele in arr:
-		heap.insert(ele)
-	return heap
-
-arr = [1,3,11,6,5,2]
-heap = get_heap(arr)
-print([ele.data for ele in heap.queue[1:]])
+		maxHeap.insert(ele)
+		minHeap.insert(ele)
+	print([ele.data for ele in maxHeap.queue[1:]])
+	print([ele.data for ele in minHeap.queue[1:]])
