4e1319cad712f941dd735d0b9307d27e54146570
LeeJuhae <tlatmsdlwngo@naver.com>
2020-09-26 23:59:04 +0900

    Modify

--- baekjoon/17142.py
+++ baekjoon/17142.py
@@ -5,42 +5,38 @@ from collections import deque
 
 def spread(boards, now_virus, min_time):
 	directions = [[-1,0],[0,1],[1,0],[0,-1]]
-	time = -1
+	time = 0
 	queue = deque(now_virus)
-	visited = copy.deepcopy(boards)
+	visited = [[0 for _ in range(N)] for _ in range(N)]
+	for virus in now_virus:
+		visited[virus[0]][virus[1]] = 1
 	while queue:
 		x, y, t = queue.popleft()
-		if t >= min_time:
+		if time >= min_time:
 			return min_time
-		visited[x][y] = 1
 		for d in directions:
-			new_x, new_y = [i+j for i,j in zip(d,[x,y])]
-			if 0 <= new_x < N and 0 <= new_y < N and boards[new_x][new_y] == 0:
-				boards[new_x][new_y] = 2
-				queue.append([new_x, new_y, t + 1])
-		time = t
-	for row in visited:
-		if row.count(0) > 0:
+			new_x, new_y = d[0] + x, d[1] + y
+			if 0 <= new_x < N and 0 <= new_y < N:
+				if boards[new_x][new_y] != 1 and visited[new_x][new_y] == 0:
+					if boards[new_x][new_y] != 2:
+						time = t + 1
+					boards[new_x][new_y] = 2
+					visited[new_x][new_y] = 1
+					queue.append([new_x, new_y, t + 1])
+	for row in boards:
+		if 0 in row:
 			return -1
 	return time
 
 def select_virus(N, M, boards, candi_virus):
-	comb_inactive_virus = list(combinations(candi_virus, len(candi_virus)-M))
+	comb_inactive_virus = list(combinations(candi_virus, M))
 	min_time = float('inf')
 	for selected_virus in comb_inactive_virus:
 		new_boards = copy.deepcopy(boards)
-		now_virus = []
-		for virus in candi_virus:
-			if virus not in selected_virus:
-				now_virus.append(virus + [0])
-			else:
-				new_boards[virus[0]][virus[1]] = '*'
-		time = spread(new_boards, now_virus, min_time)
+		time = spread(new_boards, selected_virus, min_time)
 		if time != -1:
 			min_time = time
-	if min_time == float('inf'):
-		min_time = -1
-	return min_time
+	return min_time if min_time != float('inf') else -1
 
 N, M = map(int, sys.stdin.readline().split())
 boards = []
@@ -49,9 +45,7 @@ for row in range(N):
 	boards.append(list(map(int ,sys.stdin.readline().split())))
 	for col in range(N):
 		if boards[row][col] == 2:
-			candi_virus.append([row, col])
-		elif boards[row][col] == 1:
-			boards[row][col] = '-'
+			candi_virus.append([row, col, 0])
 print(select_virus(N, M, boards, candi_virus))
 
 
