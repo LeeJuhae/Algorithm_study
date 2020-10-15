5eec9dd88ea098b45218990042e21368f45bcc08
LeeJuhae <tlatmsdlwngo@naver.com>
2020-10-15 20:17:35 +0900

    Add comments

--- baekjoon/16236.py
+++ baekjoon/16236.py
@@ -3,7 +3,7 @@ from collections import deque
 
 def find_fish(boards, shark, size, cnt):
 	directions = [[-1,0],[0,-1],[0,1],[1,0]]
-	queue = deque([[shark, 0, size, cnt]])
+	queue = deque([[shark, 0, size, cnt]]) # [상어 위치, 소요 시간, 상어 크기, 잡아 먹은 같은 크기의 물고기 수]
 	visited = [[0 for _ in range(N)] for _ in range(N)]
 	visited[shark[0]][shark[1]] = 1
 	min_x, min_y = N, N
@@ -22,14 +22,13 @@ def find_fish(boards, shark, size, cnt):
 						queue.append([[new_x, new_y], time + 1, size, cnt])
 					elif time + 1 <= min_time:
 						if new_x < min_x or (new_x == min_x and new_y < min_y):
-							min_x = new_x
-							min_y = new_y
+							min_x, min_y = new_x, new_y
 							min_time = time + 1
 							now_cnt = cnt + 1
+							# 상어 크기 변경
 							if cnt + 1 == size:
 								now_size += 1
 								now_cnt = 0
-
 	fish = [min_x, min_y]
 	if fish == [N, N]:
 		return None, 0, 0, cnt
@@ -44,9 +43,9 @@ for _ in range(N):
 	if 9 in boards[-1]:
 		loc = [len(boards)-1, boards[-1].index(9)]
 		boards[loc[0]][loc[1]] = 0
-size = 2
-all_time = 0
-cnt = 0
+size = 2 # 아기 상어 크기
+all_time = 0 # 소요 시간의 합
+cnt = 0 # 잡아 먹은 같은 크기의 물고기 수
 while True:
 	fish, time, size, cnt = find_fish(boards, loc, size, cnt)
 	if fish == None:
