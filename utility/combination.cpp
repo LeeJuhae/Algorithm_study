ca0af52b24a999cfd142e1ff5e6ee8c7aa14efc8
LeeJuhae <tlatmsdlwngo@naver.com>
2020-10-23 00:49:48 +0900

    Create my_combination function

---
+++ utility/combination.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+template <typename T>
+set<vector<T> > combination(vector<T>& arr, int k) {
+	int arr_size = arr.size();
+	set<vector<T> >ret;
+	vector<int> flag(arr_size);
+
+	for (int j = 0 ; j < k ; ++j)
+		flag[j] = 1;
+	for (int j = k ; j < arr_size ; ++j)
+		flag[j] = 0;
+	sort(flag.begin(), flag.end());
+
+	do {
+		vector<T> temp;
+		for(int i = 0; i < flag.size() ; ++i) {
+			if (flag[i])
+				temp.push_back(arr[i]);
+		}
+		ret.insert(temp);
+	} while (next_permutation(flag.begin(), flag.end()));
+	return ret;
+}
+
+// int main(){
+// 	vector <int> arr = {1,2,3,4};
+// 	set<vector<int> > ret;
+// 	ret = combination(arr, 2);
+// 	for (auto &row : ret) {
+// 		for (auto &col : row)
+// 			printf("%d ", col);
+// 		printf("\n");
+// 	}
+// 	return 0;
+// }
