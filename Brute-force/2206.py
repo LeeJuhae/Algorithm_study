eb60ffd7a8acaff6e1c3c19e12b719c1a0b6c279
LeeJuhae <tlatmsdlwngo@naver.com>
2021-07-30 15:24:11 +0900

    벽 부수고 이동하기

---
+++ Brute-force/2206.py
@@ -0,0 +1,37 @@
+# https://www.acmicpc.net/problem/2206
+
+'''
+# key point
+
+visited: [부순 벽의 수][x][y]
+'''
+import sys
+from collections import deque
+
+n, m = list(map(int, sys.stdin.readline().split()))
+maps = [list(sys.stdin.readline().split()[0]) for _ in range(n)]
+visited = [[[False for _ in range(m)] for _ in range(n)] for _ in range(2)]
+directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+q = deque([(0, 0, 1, 0)])
+visited[0][0][0] = True
+
+while q:
+	x, y, cnt, wall = q.popleft()
+	if (x, y) == (n-1, m-1):
+		print(cnt)
+		sys.exit()
+
+	for dx, dy in directions:
+		nxt_x, nxt_y = x + dx, y + dy
+
+		if nxt_x not in range(0, n) or nxt_y not in range(0, m) or visited[wall][nxt_x][nxt_y]:
+			continue
+
+		if maps[nxt_x][nxt_y] == '0':
+			q.append((nxt_x, nxt_y, cnt + 1, wall))
+			visited[wall][nxt_x][nxt_y] = 1
+		elif maps[nxt_x][nxt_y] == '1' and wall < 1 and not visited[wall+1][nxt_x][nxt_y]:
+			q.append((nxt_x, nxt_y, cnt + 1, wall + 1))
+			visited[wall+1][nxt_x][nxt_y] = 1
+print(-1)
+
