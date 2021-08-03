59fd59d8ccc3c3be597a6a1b7d15044be4c8ad82
LeeJuhae <tlatmsdlwngo@naver.com>
2021-08-03 14:35:28 +0900

    두 용액

---
+++ Binary-Search/2470.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/2470
+
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+numbers = sorted(list(map(int, sys.stdin.readline().split())))
+
+left, right = 0, n - 1
+ret = [numbers[left], numbers[right]]
+while left < right:
+	sum_val = numbers[left] + numbers[right]
+	if abs(sum_val) < abs(sum(ret)):
+		ret = [numbers[left], numbers[right]]
+		if abs(sum_val) == 0:
+			break
+	if sum_val < 0: left += 1
+	else: right -= 1
+print(ret[0], ret[1])
